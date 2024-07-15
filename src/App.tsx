a493be654ab638fb8af6671df92974161c1bf2e9
Phuoc Dinh <phuocdinh462@gmail.com>
2024-07-16 02:18:27 +0700

    feat: update main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+// import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <div className="h-screen bg-[url('./src/assets/fv_main_bg.jpg')] bg-cover bg-center">
+        <div className="flex flex-col items-center h-full">
+          <img
+            src="./src/assets/anemoi_logo.png"
+            alt="anemoi_logo.png"
+            className="object-cover h-44 mt-5"
+          />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
