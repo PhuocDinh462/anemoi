98c674e3f7564104ca8b35008ca53338bffacfeb
Phuoc Dinh <phuocdinh462@gmail.com>
2024-08-05 06:26:49 +0700

    feat: handle scroll to selected section in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,31 @@ import Navbar from "./components/navbar";
 import Intro from "./components/intro";
 import Story from "./components/story";
 import Character from "./components/character";
+import { createRef } from "react";
 
 function App() {
+  const sections = ["top", "introduction", "story", "character", "movie"];
+  const refs: React.RefObject<HTMLDivElement>[] = sections.map(() => createRef());
+
   return (
-    <>
+    <div onDragStart={(e) => e.preventDefault()}>
       <div className="w-full fixed z-[99]">
-        <Navbar />
+        <Navbar sections={sections} refs={refs} />
       </div>
-      <Cover />
-      <Intro />
-      <Story />
-      <Character />
-    </>
+      <div ref={refs[0]}>
+        <Cover />
+      </div>
+      <div ref={refs[1]}>
+        <Intro />
+      </div>
+      <div ref={refs[2]}>
+        <Story />
+      </div>
+      <div ref={refs[3]}>
+        <Character />
+      </div>
+      <div ref={refs[4]}></div>
+    </div>
   );
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,31 +1,41 @@
-export default function Navbar() {
+import React from "react";
+
+export default function Navbar(props: {
+  sections: string[];
+  refs: React.RefObject<HTMLDivElement>[];
+}) {
+  const { sections, refs } = props;
+
+  const scrollToSelectedSection = (index: number) => {
+    if (refs[index].current) {
+      refs[index].current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div
-      className="pt-2 pb-5 bg-gradient-to-b from-custom-blue-100 to-transparent 
+      className="relative pt-2 pb-5 bg-gradient-to-b from-custom-blue-100 to-transparent 
       flex items-center justify-center select-none text-center text-white font-semibold
       font-seasons tracking-widest text-[1em]"
     >
-      |
-      <a href="#" className="font-thin px-[30px]">
-        top
+      <a
+        href="#"
+        className="w-[6%] h-full absolute left-0 bg-white py-[.4%] px-[.8%] ml-[1%]
+        flex justify-center items-center"
+      >
+        <img src="src/assets/images/anemoi_logo_bk.png" sizes="100%" />
       </a>
-      |
-      <a href="#" className="font-thin px-[30px]">
-        introduction
-      </a>
-      |
-      <a href="#" className="font-thin px-[30px]">
-        story
-      </a>
-      |
-      <a href="#" className="font-thin px-[30px]">
-        character
-      </a>
-      |
-      <a href="#" className="font-thin px-[30px]">
-        movie
-      </a>
-      |
+      <div className="flex items-center justify-center w-full">
+        |
+        {sections.map((section, index) => (
+          <React.Fragment key={index}>
+            <div className="font-thin px-[30px]" onClick={() => scrollToSelectedSection(index)}>
+              {section}
+            </div>
+            |
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
