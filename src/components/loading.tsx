aad8b953a114c465b2a2ddbb28df9c688b96d034
Phuoc Dinh <phuocdinh462@gmail.com>
2024-08-15 21:22:15 +0700

    style: handle loading in xs size

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -5,11 +5,13 @@ export function Loading() {
     <div className="w-full h-screen flex items-center justify-center relative select-none">
       <div className="size-[300px] relative">
         <img
-          className="absolute w-[300px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+          className="absolute xs:w-[300px] left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
+          w-[200px]"
           src={anemoi_logo_anime2}
         />
         <img
-          className="absolute w-[180px] left-[32%] top-[15%] animate-spin-slow"
+          className="absolute xs:w-[180px] xs:left-[32%] xs:top-[15%] animate-spin-slow
+          w-[120px] left-[38%] top-[26%]"
           src={anemoi_logo_anime1}
         />
       </div>
