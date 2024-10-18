0ca2c7c4dde7bb166656b7f945ce1a566af885ff
Phuoc Dinh <phuocdinh462@gmail.com>
2024-10-18 14:12:48 +0700

    refactor: Add language selection menu to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Fade as Hamburger } from 'hamburger-react';
-import { Drawer } from '@mui/material';
+import { alpha, Drawer, IconButton, styled } from '@mui/material';
 import { anemoi_logo_bk, language_ic } from '@/constants/images';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
+
+import Menu, { MenuProps } from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { anemoi_logo_anime1 } from '@/constants/images';
 
 export default function Navbar(props: {
   sections: string[];
@@ -61,7 +65,55 @@ export default function Navbar(props: {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
-  ///
+  /// End for mobile only
+
+  const StyledMenu = styled((props: MenuProps) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center'
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    '& .MuiPaper-root': {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      color: 'rgb(55, 65, 81)',
+      boxShadow:
+        'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      '& .MuiMenu-list': {
+        padding: '4px 0'
+      },
+      '& .MuiMenuItem-root': {
+        '& .MuiSvgIcon-root': {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5)
+        },
+        '&:active': {
+          backgroundColor: alpha(theme.palette.primary.main, theme.palette.action.selectedOpacity)
+        }
+      },
+      ...theme.applyStyles('dark', {
+        color: theme.palette.grey[300]
+      })
+    }
+  }));
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const openLanguageMenu = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className="w-screen max-bm:relative">
@@ -108,7 +160,7 @@ export default function Navbar(props: {
 
       {/* PC case */}
       <div
-        className="relative size-full max-bm:hidden pt-[10px] pb-[15px]
+        className="relative size-full max-bm:hidden pt-[15px] pb-[20px]
         bg-gradient-to-b from-custom-blue-100 to-transparent
         flex items-center justify-center select-none text-center font-semibold
         tracking-widest text-[1em] font-seasons text-white">
@@ -137,11 +189,37 @@ export default function Navbar(props: {
           ))}
         </div>
 
-        <img
-          className="cursor-pointer py-4 size-14 absolute right-3
-          hover:scale-110 transition-all duration-300"
-          src={language_ic}
-        />
+        <div className="absolute right-3">
+          <IconButton id="change-language-button" sx={{ p: 0 }} size="small" onClick={handleClick}>
+            <img
+              className="cursor-pointer py-4 size-14
+              hover:scale-110 transition-all duration-300"
+              src={language_ic}
+            />
+          </IconButton>
+          <StyledMenu
+            id="change-language-menu"
+            MenuListProps={{
+              'aria-labelledby': 'change-language-button'
+            }}
+            disableScrollLock
+            anchorEl={anchorEl}
+            open={openLanguageMenu}
+            onClose={handleClose}>
+            <MenuItem onClick={handleClose} disableRipple>
+              <div className="flex items-center justify-between w-full">
+                <div className="font-tsukumin">日本語</div>
+                <img className="ml-5" src={anemoi_logo_anime1} width={24} />
+              </div>
+            </MenuItem>
+            <MenuItem onClick={handleClose} disableRipple>
+              <div className="flex items-center justify-between w-full">
+                <div className="">Tiếng Việt</div>
+                <img className="ml-5" src={anemoi_logo_anime1} width={24} />
+              </div>
+            </MenuItem>
+          </StyledMenu>
+        </div>
       </div>
     </div>
   );
