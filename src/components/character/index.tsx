99d9855d3e396983a975aca22a5d3967d270ee2c
Phuoc Dinh <phuocdinh462@gmail.com>
2024-12-12 11:45:37 +0700

    style: update margin in Character component for improved layout

--- src/components/character/index.tsx
+++ src/components/character/index.tsx
@@ -55,7 +55,7 @@ export default function Character() {
         <div className="font-thomasita text-[#4ddaf2] font-semibold text-lg">anemoi</div>
       </div>
 
-      <div className={`max-xl:absolute xl:mb-[3%] size-full z-20 ${isInView && 'animate-fadeUp'}`}>
+      <div className={`max-xl:absolute xl:my-[3%] size-full z-20 ${isInView && 'animate-fadeUp'}`}>
         <CharacterBox />
       </div>
 
